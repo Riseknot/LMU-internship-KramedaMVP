67a1f8d025dca99d10bc7772bb5e5ee6b96b5f1c
Riseknot <mr.shockwave@live.de>
2026-04-09 16:37:21 +0200

    Change of Logo-Design

--- app/signIn & signUp/LoginPage.tsx
+++ app/signIn & signUp/LoginPage.tsx
@@ -60,7 +60,13 @@ export function LoginPage({ onLogin, onShowRegister }: LoginPageProps) {
       <div className="auth-shell-inner w-full max-w-md">
         {/* Logo and Header */}
         <div className="text-center mb-6">
-            <AppLogo />
+             
+            <div className="relative inline-block">
+              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-amber-200/30 via-sky-300/25 to-violet-300/30 blur-2xl" />
+              <h1 className="relative bg-gradient-to-r from-amber-100 via-sky-200 to-violet-200 bg-clip-text pl-[0.15em] text-7xl font-black tracking-[0.36em] text-transparent drop-shadow-[0_0_24px_rgba(126,220,255,0.18)] sm:text-9xl">
+                LUMI
+              </h1>
+            </div>
             <Slogan />
         </div>
 
--- app/src/components/AppLogo.tsx
+++ app/src/components/AppLogo.tsx
@@ -3,10 +3,11 @@ import LumiLogo from "@/src/assets/Lumi_Logo_Blue.png";
 
 export function AppLogo() {
   return (
-    <Image
-      src={LumiLogo}
-      alt="Logo of the App"
-    priority={true}   
-    />
+                <div className="relative inline-block">
+              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-amber-200/30 via-sky-300/25 to-violet-300/30 blur-2xl" />
+              <h1 className="relative bg-gradient-to-r from-amber-100 via-sky-200 to-violet-200 bg-clip-text pl-[0.15em] text-xl font-black tracking-[0.36em] text-transparent drop-shadow-[0_0_24px_rgba(126,220,255,0.18)] ">
+                LUMI
+              </h1>
+            </div>
   );
 }
